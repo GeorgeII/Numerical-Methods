bde0f2f387b37225f06b511057aef0ab4c54df3f
GeorgeII windows <egorzorikoff@yandex.ru>
2018-11-24 17:16:10 +0400

    Finite difference

--- Finite Difference Method/Main.py
+++ Finite Difference Method/Main.py
@@ -1,28 +1,28 @@
 import numpy as np
-import pandas
-from tabulate import tabulate
+import methods as ms
 
 a = 0
 b = 1
 h = 0.1
 n = int((b - a) / h + 1)
 x = np.linspace(a, b, n)
-y = np.array([])
-boundaryConditions = [[0, 1],
-                      [0, -1]]
+y = np.zeros(n)
+# boundary conditions
+y[0] = 0
+y[n-1] = -1
 
+# tridiagonal matrix algorithm
+A = np.ones(n)
+C = np.ones(n)
+B = np.full(n, -2 - h**2)
+D = 2*x * h**2
+MU = np.zeros(n)
+V = np.copy(y)
+for i in range(1, n-1):
+    MU[i] = -C[i] / (A[i]*MU[i-1] + B[i])
+    V[i] = (D[i] - A[i] * V[i - 1]) / (A[i] * MU[i - 1] + B[i])
+for i in range(n-2, 0, -1):
+    y[i] = MU[i]*y[i+1]+V[i]
 
-
-
-
-
-yPreciseSolution = np.sinh(x) / np.sinh(1) - 2 * x
-
-# printing the table
-df = pandas.DataFrame({"x": x, "yPrecise": yPreciseSolution, "z": x, "asd": yPreciseSolution})
-print(df)
-
-col_headers = ["x", "y", "z"]
-merged_array = np.array([x, yPreciseSolution, x]).T
-table = tabulate(merged_array , col_headers, tablefmt="fancy_grid")
-print(table)
+y_precise_solution = np.sinh(x) / np.sinh(1) - 2 * x
+ms.print_tables(x, y, y_precise_solution)
--- Finite Difference Method/Methods.py
+++ Finite Difference Method/Methods.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas
-import tabulate
+from tabulate import tabulate
 
 
-def preciseFunc(x):
-    yPreciseSolution = np.sinh(x) / np.sinh(1) - 2 * x
-    print(x, yPreciseSolution)
+def print_tables(x, y, y_precise_solution):
+    table1 = pandas.DataFrame({"x": x, "Precise y": y_precise_solution, "y": y, "Error": abs(y - y_precise_solution)})
+    table1 = table1.round(6)
+    print(table1)
 
+    # tabulated output
+    col_headers = ["x", "Precise y", "y", "Error"]
+    merged_array = np.array([x, y_precise_solution, y, abs(y - y_precise_solution)]).T
+    table2 = tabulate(merged_array , col_headers, tablefmt="fancy_grid", floatfmt = ".6f")
+    print(table2)
+
+    print("Maximum error: ", format(np.max(abs(y - y_precise_solution)), '.6f'))
