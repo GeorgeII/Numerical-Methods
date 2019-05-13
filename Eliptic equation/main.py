e784cff5fd97b7f2fbd91e8e65c9f6d5cf408e6c
GeorgeII <egorzorikov@gmail.com>
2019-05-13 22:59:16 +0400

    Fix some bugs and complete the task

--- Eliptic equation/main.py
+++ Eliptic equation/main.py
@@ -5,7 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def main():
-    x_number, t_number = 5, 4
+    x_number, t_number = 20,20
 
     # boundaries
     t_min, t_max = 0, 1
@@ -31,38 +31,62 @@ def main():
     y[:, -1] = 30 * x * (1 - x)
 
     np.set_printoptions(precision=2)
-    print("Boundary conditions were set: \n{}".format(y))
+    print("Boundary conditions are set: \n{}".format(y))
 
     a = -1 / h**2
     b = -1 / tau**2
-    c = 2 / h**2 + 2 / h**2
+    c = 2 / h**2 + 2 / tau**2
 
-    matrix_size = (x_number-1) * (t_number-1)
+    matrix_size = (x_number-2) * (t_number-2)
     A = np.zeros([matrix_size, matrix_size])
 
-    # in my case function is always 0
-    f = np.zeros(matrix_size)
-
     np.fill_diagonal(A, c)
-
     i, j = np.indices(A.shape)
-    A[i==j] = c
-    A[i==j-x_number+1] = b
-    #A[i==j-1] = a
+    A[i == j-(x_number-2)] = b
+    A[i == j-1] = a
     for i in range(matrix_size-1):
-        if (i+1) % (x_number-1) == 0:
-            continue
-        else:
-            A[i][i+1] = a
-
+        if (i+1) % (x_number-2) == 0:
+            A[i][i+1] = 0
+    # symmetrize the matrix
+    A = A + A.T - np.diag(A.diagonal())
     print(A)
-    print(matrix_size)
-    print(int((matrix_size-1) / (t_number-1)))
 
+    # in my case function is always 0
+    f = np.zeros([x_number-2, t_number-2])
+    # vector F in linear system: Ay = F
+    F = np.zeros([x_number-2, t_number-2])
 
-'''
-    print('Result:\n{}'.format(y))
-    plot_matrix(x, t, y)'''
+    for i in range(x_number-2):
+        for j in range(t_number-2):
+            if i == 0:
+                F[i][j] -= a * y[0][j+1]
+            if i == x_number - 3:
+                F[i][j] -= a * y[-1][j+1]
+            if j == 0:
+                # this condition might be harmful
+                if i == 0 or i == x_number - 3:
+                    F[i][j] += b * y[i + 1][0]
+                else:
+                    F[i][j] -= b * y[i+1][0]
+            if j == t_number - 3:
+                # this condition might be harmful
+                if i == 0 or i == x_number - 3:
+                    F[i][j] += b * y[i + 1][-1]
+                else:
+                    F[i][j] -= b * y[i+1][-1]
+        F[i][j] += f[i][j]
+    F = F.flatten()
+
+    result = np.linalg.solve(A, F)
+    result = np.reshape(result, (x_number-2, t_number-2))
+
+    for i in range(1, x_number-1):
+        for j in range(1, t_number-1):
+            y[i][j] = result[i-1][j-1]
+
+    print(result)
+    print(y)
+    plot_matrix(x, t, y)
 
 
 def plot_matrix(x, t, y):
